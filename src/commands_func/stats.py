42b6e61f3390f7a3fb518c440d5422d0b6124156
Leo Hemmingsson <leohemmingsson@gmail.com>
2022-09-09 09:57:36 +0200

    formating looks good again in stats

--- src/commands_func/stats.py
+++ src/commands_func/stats.py
@@ -12,7 +12,7 @@ class Stats:
             response += ("-" * (longest_name + 12)) + "\n"
 
             for counter, person in enumerate(self.memory.get_message_list()):
-                if person != "":
+                if self.memory.get_message_count(person) != "":
                     response += f"| \#{str(counter+1)} {self.memory.get_message_count(person)}"
 
             response += "-" * (longest_name + 12)
