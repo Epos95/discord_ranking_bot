e4f9fe1ccc151ac3a585690db4080e35dac67e7b
Epos95 <39768937+Epos95@users.noreply.github.com>
2022-10-18 22:25:08 +0200

    more updates

--- src/commands_func/mute.py
+++ src/commands_func/mute.py
@@ -11,20 +11,7 @@ class Mute:
         self.admins = ["Epos95", "CurryAndMilk"]
         self.GOD_MODE = True  # Kill switch for if it gets annoying
 
-    async def __timeout(self, message, muted_user, timeout=10):
-        if self.GOD_MODE and muted_user.name in self.admins:
-            snarky_messages = [
-                "Oh yea please try again",
-                "you thought!",
-                "...",
-                "Going good?",
-                "Error!",
-                "woops, looks like you tried to mute a admin, would you like some help with that?",
-                "OwO I did a fucky wucky uwu",
-            ]
-
-            await message.channel.send(choice(snarky_messages))
-            return
+    async def __timeout_user(self, message, muted_user, timeout=10):
 
         await asyncio.sleep(timeout)
 
@@ -35,14 +22,42 @@ class Mute:
     async def mute_user(self, message):
         if self.__get_mentions(message):
             user = self.__get_mentions(message)
-            self.muted.append(user.id)
 
+            if self.GOD_MODE and user.name in self.admins:
+                snarky_messages = [
+                    "Oh yea please try again",
+                    "you thought!",
+                    "...",
+                    "Going well?",
+                    "Error!",
+                    "woops, looks like you tried to mute a admin, would you like some help with that?",
+                    "OwO I did a fucky wucky uwu",
+                    "lol",
+                    "rukd",
+                    "Absolutely decimated",
+                    "Git gud",
+                    "cargo install skills",
+                    "no",
+                    "i refuse",
+                ]
+
+                await message.channel.send(choice(snarky_messages))
+                return
+
+            timeout = 10
+            if self.GOD_MODE and message.author.name in self.admins:
+                try:
+                    timeout = int(message.content.split(" ")[2])
+                except:
+                    pass
+
+            self.muted.append(user.id)
             await message.channel.send(
-                f"Successfully muted {user.name} for {self.timeout} seconds!"
+                f"Successfully muted {user.name} for {timeout} seconds!"
             )
 
             # Add a timeout for each mute, as a safe guard
-            await asyncio.create_task(self.__timeout(message, user))
+            await asyncio.create_task(self.__timeout_user(message, user, timeout))
 
         else:
             await message.channel.send(f"Failed to mute...")
